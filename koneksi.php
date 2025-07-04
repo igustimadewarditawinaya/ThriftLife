edebe8db84bbf9616e64d3d7bc78d4817522dd10
igustimadewarditawinaya <aditwinaya123@gmail.com>
2025-07-04 23:58:33 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,28 @@
-<?php $koneksi = new mysqli('localhost','root','','final_marketplace'); ?>
+<?php
+if (getenv('CLEARDB_DATABASE_URL')) {
+    // Koneksi Heroku
+    $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    try {
+        $koneksi = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $username, $password);
+        $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Koneksi gagal: " . $e->getMessage());
+    }
+} else {
+    // Koneksi lokal
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "final_marketplace";
+    try {
+        $koneksi = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $username, $password);
+        $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Koneksi gagal: " . $e->getMessage());
+    }
+}
+?>
